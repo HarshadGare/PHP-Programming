7745063e56dcd30d80a221f9b4a5720de550633b
HarshadGare <30696721+HarshadGare@users.noreply.github.com>
2019-09-26 10:27:05 +0530

    Add files via upload

---
+++ 01 echo and print.php
@@ -0,0 +1,17 @@
+<?php
+     $name='Harshad'; 
+	 
+	 echo "Hello $name <br>"; //use of "" 
+	 
+	 echo 'Hello $name <br>';  //use of '' 
+
+	 echo 'Hello'.$name;  //concatination
+	 
+	 print "<br>Using print function<br>";
+	 
+	 //Diferrence between echo and print given below
+	 
+	 echo "Hello","Harshad";
+	 
+	 //print "Hello","Harshad"; it will gives the Error
+	 ?>
